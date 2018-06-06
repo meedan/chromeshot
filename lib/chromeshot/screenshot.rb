16fd04bf9a41f5b18586ed3cb14daf1ad1ca1730
ahmid <ahd.hjz@gmail.com>
2018-06-06 13:50:25 +0300

    disabling some additional parameters that we do not need to enhance performance

--- lib/chromeshot/screenshot.rb
+++ lib/chromeshot/screenshot.rb
@@ -7,7 +7,28 @@ module Chromeshot
     attr_accessor :debug_port
 
     def self.setup_chromeshot(debug_port)
-      system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=#{debug_port} --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors &"
+      #system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=#{debug_port} --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors &"
+      system "LC_ALL=C google-chrome 
+                --headless 
+                --enable-logging 
+                --hide-scrollbars 
+                --remote-debugging-port=#{debug_port} 
+                --remote-debugging-address=0.0.0.0 
+                --disable-gpu 
+                --no-sandbox 
+                --ignore-certificate-errors 
+                --disable-default-apps
+                --disable-extensions 
+                --disable-sync 
+                --disable-translate 
+                --hide-scrollbars 
+                --metrics-recording-only
+                --mute-audio
+                --no-first-run 
+                --safebrowsing-disable-auto-update
+                --ignore-ssl-errors
+                --ignore-certificate-errors-spki-lis &"
+
     end
 
     def initialize(options = {})

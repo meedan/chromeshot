733dac04c9b0b4891d988520f395ca8318f32f49
ahmid <ahd.hjz@gmail.com>
2018-06-22 11:21:40 +0300

    Trying not to start chrome at startup

--- lib/chromeshot/screenshot.rb
+++ lib/chromeshot/screenshot.rb
@@ -8,7 +8,7 @@ module Chromeshot
 
     def self.setup_chromeshot(debug_port)
       #system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=#{debug_port} --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors &"
-      system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=#{debug_port} --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors --disable-default-apps --disable-extensions --disable-sync --disable-translate --hide-scrollbars --metrics-recording-only --mute-audio --no-first-run --safebrowsing-disable-auto-update --ignore-ssl-errors --ignore-certificate-errors-spki-lis &"
+      #system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=#{debug_port} --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors --disable-default-apps --disable-extensions --disable-sync --disable-translate --hide-scrollbars --metrics-recording-only --mute-audio --no-first-run --safebrowsing-disable-auto-update --ignore-ssl-errors --ignore-certificate-errors-spki-lis &"
 
     end
 

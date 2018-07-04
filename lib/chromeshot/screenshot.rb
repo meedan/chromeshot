a2f745033399d453b0b61024840522b39378733d
ahmid <ahd.hjz@gmail.com>
2018-07-04 13:36:41 +0300

    Debug

--- lib/chromeshot/screenshot.rb
+++ lib/chromeshot/screenshot.rb
@@ -15,9 +15,6 @@ module Chromeshot
 
     def take_screenshot(options = {})
 
-      system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=9444 --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors &"
-      sleep(30);
-
       screenshoter = File.join Chromeshot.root, 'bin', 'take-screenshot.js'
       system 'nodejs', screenshoter, "--url=#{options[:url]}", "--output=#{options[:output]}", "--delay=5", "--debugPort=#{self.debug_port}", "--full=true", "--script=#{options[:script]}"
       system 'convert', options[:output], '-trim', '-strip', '-quality', '90', options[:output]
@@ -25,6 +22,7 @@ module Chromeshot
 
     # Load page in a new tab, set the viewport size, wait a little and return the tab id
     def load_page_in_new_tab(options = {})
+
       system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=9444 --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors &"
       sleep(30);
 
@@ -36,9 +34,6 @@ module Chromeshot
     # Take the screenshot of a page that is already loaded
     def take_screenshot_from_tab(options = {})
 
-      system "LC_ALL=C google-chrome --headless --enable-logging --hide-scrollbars --remote-debugging-port=9444 --remote-debugging-address=0.0.0.0 --disable-gpu --no-sandbox --ignore-certificate-errors &"
-      sleep(30);
-      
       screenshoter = File.join Chromeshot.root, 'bin', 'save-screenshot.js'
       system 'nodejs', screenshoter, "--tab=#{options[:tab]}", "--output=#{options[:output]}", "--debugPort=#{self.debug_port}", "--script=#{options[:script]}"
       system 'convert', options[:output], '-trim', '-strip', '-quality', '90', options[:output]
